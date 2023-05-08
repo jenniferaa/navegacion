82ec135df2506f55659b827d1799ac5fd861fb76
jenniferaa <128671261+jenniferaa@users.noreply.github.com>
2023-05-08 16:51:13 -0600

    Initial commit

---
+++ src/app/components/pagina1/pagina1.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Router} from '@angular/router';
+
+@Component({
+  selector: 'app-pagina1',
+  templateUrl: './pagina1.component.html',
+  styleUrls: ['./pagina1.component.css']
+})
+export class Pagina1Component{
+
+ constructor(private router: Router) {}
+ 
+
+ navegar(){
+ this.router.navigate(['/pagina2']);
+ }
+}
